7bacbd9c803bd32025472e0aef7d2130e6b601f4
Dranatom <zorg.anatom1990@gmail.com>
2021-02-19 17:23:36 +0200

    refactor method countUniqueCharacters

--- src/main/java/ua/com/foxminded/UniqueSymbolsCounter.java
+++ src/main/java/ua/com/foxminded/UniqueSymbolsCounter.java
@@ -1,17 +1,20 @@
 package ua.com.foxminded;
 
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class UniqueSymbolsCounter {
-    private static final Map<String, Map<Character, Integer>> cashe = new HashMap<>();
+    private static final Map<String, Map<Character, Integer>> cache = new HashMap<>();
 
     public String countNumberOfUniqueCharacters(String inputString) {
         if (validateDataAndCheckHash(inputString).isPresent()) {
             return formatStringResult(validateDataAndCheckHash(inputString).get());
         }
         Map<Character, Integer> uniqueCharsCounts = countUniqueCharacters(inputString);
-        cashe.put(inputString, uniqueCharsCounts);
+        cache.put(inputString, uniqueCharsCounts);
         return formatStringResult(uniqueCharsCounts);
     }
 
@@ -22,20 +25,15 @@ public class UniqueSymbolsCounter {
         if (inputString.isEmpty()) {
             throw new IllegalArgumentException("Input is empty. String isn`t contains unique elements!");
         }
-        if (cashe.containsKey(inputString)) {
-            return Optional.of(cashe.get(inputString));
+        if (cache.containsKey(inputString)) {
+            return Optional.of(cache.get(inputString));
         }
         return Optional.empty();
     }
 
     private Map<Character, Integer> countUniqueCharacters(String inputString) {
         Map<Character, Integer> result = new LinkedHashMap<>();
-        char[] elementsOfString = inputString.toCharArray();
-        for (int i = 0; i < elementsOfString.length; i++) {
-            result.merge(elementsOfString[i], 1, Integer::sum);
-        }
-//        List<String> elementsOfString = Arrays.asList(inputString.split(""));
-//        elementsOfString.forEach(element -> result.merge(element, 1, (oldVal, newVal) -> oldVal + newVal));
+        inputString.chars().forEach(element -> result.merge((char)element, 1, Integer::sum));
         return result;
     }
 
